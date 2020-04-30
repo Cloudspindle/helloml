490ff01c76dd91b6e7c2b6d4231580128ea7e5e5
george redinger <george@georgeredinger.com>
2020-04-30 11:09:32 -0700

    functional vs global  net

--- helloml.py
+++ helloml.py
@@ -9,10 +9,21 @@
 #--------------------------------------------------------------------------------
 !git clone https://github.com/Cloudspindle/helloml.git
 from helloml.ml import init,load_data,show_input_example,define_neural_net,show_input_example,define_neural_net,train_neural_net,test_neural_net
+input_set_size=100
+test_set_size=2
+num_samples=200               # 200 samples are in the motion examples
+
+# neural network node parameters
+num_inputs=num_samples        # we make the number of neural inputs equal to the number of samples in the motion examples
+num_hidden=512
+num_outputs=2
+training_cycles=100
+
 init() #define global variables
 (input_set,output_set,test_set)=load_data() #load the data (input training, output and test) NB - You manually #now need to load the following files in this order (ClockwiseZero_accel.200.csv, AntiClockwiseZero_accel.200.csv, ChloeClockwise_accel.200.csv, ChloeAntiClock_accel.200.csv). The first two are the training examples from which a full 100 example #training set will be built. The second two and the two examples #that will be tested after training.
 show_input_example() #show an example of the data (input training v test) - rerun to #randomly select and example
+
 net=define_neural_net(num_inputs,num_hidden,num_outputs) #define the neural network
 train_neural_net(net,training_cycles) #train the neural network
-test_neural_net() #test the neural network with the two test examples (the blue signal line is the clockwise test example and the orange signal line is the anticlockwise test example)
+test_neural_net(net) #test the neural network with the two test examples (the blue signal line is the clockwise test example and the orange signal line is the anticlockwise test example)
 !rm -rf helloml
