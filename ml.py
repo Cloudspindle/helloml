5f14109650d3ebf33a479631732a8e3228097cfa
george redinger <george@georgeredinger.com>
2020-04-30 10:58:02 -0700

    more globals

--- ml.py
+++ ml.py
@@ -134,7 +134,7 @@ def train_neural_net(net,training_cycles):
 
 def test_neural_net():
   global input_set,output_set,test_set
-  
+  global net
   plt.figure(figsize=(10,5))
   plt.plot(test_set[0])
   plt.plot(test_set[1])
